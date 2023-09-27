ed92e933b2ff60abf175211db5b0e6498ba8cb82
juanchomarengo <juanmmarengo@gmail.com>
2023-09-27 14:47:56 -0300

    Added a comment to express-async-errors imports, to remind developers to import that library in all controllers. As well updated the description of a task in the readme file

--- node/express-prisma-typescript/src/domains/post/controller/post.controller.ts
+++ node/express-prisma-typescript/src/domains/post/controller/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express'
 import HttpStatus from 'http-status'
+// express-async-errors is a module that handles async errors in express, don't forget import it in your new controllers
 import 'express-async-errors'
 
 import { db, BodyValidation } from '@utils'
